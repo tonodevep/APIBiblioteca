325e075b60784347bcb406129ef0ce8fa485fc42
Juniuun <sarafox_2006@hotmail.com>
2025-05-20 22:27:54 +0100

    Update LibroController.java
    Añadiendo más comentarios.

--- src/main/java/com/example/biblioteca/controller/LibroController.java
+++ src/main/java/com/example/biblioteca/controller/LibroController.java
@@ -1,5 +1,6 @@
 package com.example.biblioteca.controller;
 
+//Imports
 import com.example.biblioteca.model.Libro;
 import com.example.biblioteca.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,28 +11,32 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+//Anotación de la ruta de libros
 @RequestMapping("/api/v1/libros")
 public class LibroController {
     @Autowired
     private LibroService libroService;
 
+    //Obtener todos los libros
     @GetMapping
     public List<Libro> getAllLibros() {
         return libroService.findAll();
     }
 
+    //Obtener libros por id
     @GetMapping("/{id}")
     public ResponseEntity<Libro> getLibroById(@PathVariable Long id) {
         Optional<Libro> libro = libroService.findById(id);
         return libro.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    //Crear libro
     @PostMapping
     public Libro createLibro(@RequestBody Libro libro) {
         return libroService.save(libro);
     }
 
-
+    //Actualizar libro
     @PutMapping("/{id}")
     public ResponseEntity<Libro> updateLibro(@PathVariable Long id, @RequestBody Libro libroDetails) {
         Optional<Libro> libroOptional = libroService.findById(id);
@@ -49,6 +54,7 @@ public class LibroController {
         return ResponseEntity.ok(updatedLibro);
     }
 
+    //Borrar libro
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLibro(@PathVariable Long id) {
         if (libroService.findById(id).isEmpty()) {
@@ -58,6 +64,7 @@ public class LibroController {
         return ResponseEntity.noContent().build();
     }
 
+    //Buscar libro
     @GetMapping("/buscar")
     public List<Libro> buscarLibros(
             @RequestParam(required = false) String titulo,
